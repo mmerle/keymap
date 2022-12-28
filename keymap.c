96226dd7823be677ca305a7495443eb38a87f857
mmerle <35971019+mmerle@users.noreply.github.com>
2022-12-28 13:33:09 -0500

    add wip game layer

--- keymap.c
+++ keymap.c
@@ -2,10 +2,16 @@
 #include "muse.h"
 
 enum planck_layers {
-  _BASE,
+  _BASE = 0,
   _SYM,
   _NUM,
   _ADJUST,
+  _GAME,
+};
+
+enum custom_keycodes {
+  GAME_ON,
+  GAME_OFF,
 };
 
 enum planck_keycodes {
@@ -90,15 +96,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
   │     │     │     │     │     │     │     │     │     │     │     │     │
   ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
-  │     │     │     │     │ ▓▓▓ │           │ ▓▓▓ │     │     │     │     │
+  │     │     │     │     │ ▓▓▓ │           │ ▓▓▓ │     │     │     │GAME │
   └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
 */
 
 [_ADJUST] = LAYOUT_planck_grid(
   _______, KC_BRMD, KC_BRMU, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   QK_BOOT, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, KC_MPLY,
-  _______, _______, _______, _______, _______, RGB_TOG, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TG(_GAME)
+),
+
+/* [_GAME] - game
+  ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+  │ tab │  q  │  w  │  e  │  r  │  t  │  y  │  u  │  i  │  o  │  p  │ bsp │
+  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+  │ esc │  a  │  s  │  d  │  f  │  g  │  h  │  j  │  k  │  l  │     │ ent │
+  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+  │ sft │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │ sft │
+  ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
+  │     │     │     │ ctl │     │   space   │     │     │     │     │ EXT │
+  └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
+*/
+
+[_GAME] = LAYOUT_planck_grid(
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC,
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,     KC_9,    KC_0,    KC_RSFT,
+  XXXXXXX, XXXXXXX, KC_LALT, KC_LCTL, _______, KC_SPC,  KC_SPC, _______, KC_RGUI,  KC_LALT, XXXXXXX, TG(_GAME)
 ),
 
 };
@@ -108,28 +133,24 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _NUM, _SYM, _ADJUST);
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case BACKLIT:
-      if (record->event.pressed) {
-        register_code(KC_RSFT);
-        #ifdef BACKLIGHT_ENABLE
-          backlight_step();
-        #endif
-        #ifdef KEYBOARD_planck_rev5
-          writePinLow(E6);
-        #endif
-      } else {
-        unregister_code(KC_RSFT);
-        #ifdef KEYBOARD_planck_rev5
-          writePinHigh(E6);
-        #endif
-      }
-      return false;
-      break;
-  }
-  return true;
-}
+// macro definitions
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   switch (keycode) {
+//   case GAME_ON: // enable game
+//     if (record->event.pressed) {
+//       layer_on(_GAME);
+//     }
+//     return false;
+//     break;
+//   case GAME_OFF: // disable game
+//     if (record->event.pressed) {
+//       layer_off(_GAME);
+//     }
+//     return false;
+//     break;
+//   }
+//   return true;
+// };
 
 bool muse_mode = false;
 uint8_t last_muse_note = 0;
